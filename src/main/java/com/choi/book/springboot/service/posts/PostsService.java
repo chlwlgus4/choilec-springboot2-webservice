daf3c38fc7d65e8caea504a32b3935c75214af23
chlwl <chlwlgus89@naver.com>
2019-12-14 17:57:01 +0900

    PostsApiControllerTest 안됨 ㅡ ㅡ!

---
+++ src/main/java/com/choi/book/springboot/service/posts/PostsService.java
@@ -0,0 +1,18 @@
+package com.choi.book.springboot.service.posts;
+
+import com.choi.book.springboot.domain.posts.PostsRepository;
+import com.choi.book.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
