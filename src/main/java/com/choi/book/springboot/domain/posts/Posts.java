a042e63c439b0749fb5f630acf9cd3a476752f0f
chlwl <chlwlgus89@naver.com>
2019-12-14 16:19:58 +0900

    게시글저장 TEST까지

---
+++ src/main/java/com/choi/book/springboot/domain/posts/Posts.java
@@ -0,0 +1,33 @@
+package com.choi.book.springboot.domain.posts;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor //기본 생성자 자동추가
+@Entity // 테이블과 링크될 클래스임을 나타냄
+public class Posts {
+
+    @Id //해당 테이블의 pk 필드를 나타냄
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //PK규칙을 나타냄
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder // 해당 클래스의 빌더 패턴 클래스를 생성 / 생성자 상단에 선언시 생성자에 포함된 필드만 빌더에 포함
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+}
--- src/main/java/com/choi/book/springboot/web/dto/HelloResponseDto.java
+++ src/main/java/com/choi/book/springboot/web/dto/HelloResponseDto.java
@@ -2,23 +2,12 @@ package com.choi.book.springboot.web.dto;
 
 import lombok.*;
 
-//@Getter
-//@RequiredArgsConstructor //final 필드에 만 생성자 부여
+@Getter
+@RequiredArgsConstructor //final 필드에 만 생성자 부여
 public class HelloResponseDto {
 
-    private  String name;
-    private  int amount;
+    private final String name;
+    private final int amount;
 
-    public String getName() {
-        return name;
-    }
 
-    public int getAmount() {
-        return amount;
-    }
-
-    public HelloResponseDto(String name, int amount) {
-        this.name = name;
-        this.amount = amount;
-    }
 }
