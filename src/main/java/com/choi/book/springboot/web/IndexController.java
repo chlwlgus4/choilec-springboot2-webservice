9166a83dfbb61c0e87bf11a5141eb6cf910def36
chlwl <chlwlgus89@naver.com>
2019-12-17 10:41:03 +0900

    구글 oauth 등록

--- src/main/java/com/choi/book/springboot/web/IndexController.java
+++ src/main/java/com/choi/book/springboot/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.choi.book.springboot.web;
 
+import com.choi.book.springboot.config.auth.dto.SessionUser;
+import com.choi.book.springboot.domain.user.User;
 import com.choi.book.springboot.service.posts.PostsService;
 import com.choi.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,15 +10,23 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        System.out.println("user ::::::::::::::" + user);
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
