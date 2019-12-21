3ece18e7d4dba00e219bb8b0e94ee041d803f451
chlwl <chlwlgus89@naver.com>
2019-12-21 14:47:23 +0900

    프로파일 추가

---
+++ src/test/java/com/choi/book/springboot/web/ProfileControllerUnitTest.java
@@ -0,0 +1,29 @@
+package com.choi.book.springboot.web;
+
+import com.choi.book.springboot.web.dto.ProfileController;
+import org.junit.Test;
+import org.springframework.mock.env.MockEnvironment;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ProfileControllerUnitTest {
+
+    @Test
+    public void real_profile이_조회된다() {
+        //given
+        String expectedProfile = "real";
+        MockEnvironment env = new MockEnvironment();
+        env.addActiveProfile(expectedProfile);
+        env.addActiveProfile("oauth");
+        env.addActiveProfile("real-db");
+
+        ProfileController controller = new
+                                ProfileController(env);
+
+        //when
+        String profile = controller.profile();
+
+        //then
+        assertThat(profile).isEqualTo(expectedProfile);
+    }
+}
