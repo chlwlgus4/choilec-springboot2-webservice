54cff4984466a030d3e35021198cc301c9f5a45a
chlwl <chlwlgus89@naver.com>
2019-12-29 11:52:49 +0900

    css, js추가

--- src/main/java/com/choi/book/springboot/Application.java
+++ src/main/java/com/choi/book/springboot/Application.java
@@ -8,6 +8,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
+        System.setProperty("spring.devtools.restart.enabled", "true");
+        System.setProperty("spring.devtools.livereload.enabled", "true");
         SpringApplication.run(Application.class, args);
     }
 }
