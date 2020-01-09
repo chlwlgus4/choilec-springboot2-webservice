823926f92515bf4f853a3d5a4c789b782b75ede8
chlwl <chlwlgus89@naver.com>
2020-01-09 14:36:58 +0900

    경로 수정

--- src/main/java/com/choi/book/springboot/config/auth/SecurityConfig.java
+++ src/main/java/com/choi/book/springboot/config/auth/SecurityConfig.java
@@ -21,7 +21,7 @@ SecurityConfig extends WebSecurityConfigurerAdapter {
                 .headers().frameOptions().disable() //h2-console화면을 사용하기 위해 해당 옵션들을 disable 합니다.
                 .and()
                 .authorizeRequests() // URL별 권한 관리를 설정하는 옵션의 시작점
-                .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile", "/login/**", "/register/**", "/forgotpwd/**").permitAll()
+                .antMatchers("/", "/css/**", "/images/**", "/images/slide/**", "/js/**", "/h2-console/**", "/profile", "/login/**", "/register/**", "/forgotpwd/**").permitAll()
                 //antMatchers 권한 관리 대상을 지정하는 옵션
                 //"/"등 지정된 URL들은 primitAll() 옵션을 통해 전체 열람 권한을 줌줌
                 .antMatchers("/api/v1/**").hasRole(Role.USER.name())
